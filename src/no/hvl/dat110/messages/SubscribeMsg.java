eb857b8856c24428c80f2663150ee5a402e83cf5
Sondre <songro93@gmail.com>
2021-03-06 23:32:56 +0100

    SubscribeMsg Completed

--- src/no/hvl/dat110/messages/SubscribeMsg.java
+++ src/no/hvl/dat110/messages/SubscribeMsg.java
@@ -4,10 +4,29 @@ public class SubscribeMsg extends Message {
 
 	// message sent from client to subscribe on a topic 
 
-	// TODO:
+	// COMPLETED:
 	// Implement object variables - a topic is required
 
 	// Constructor, get/set-methods, and toString method
 	// as described in the project text
+	
+	//====================================================================
+	
+		private String topic;
+		
+		public SubscribeMsg(String user, String topic) {
+			super(MessageType.SUBSCRIBE, user);
+			this.topic = topic;
+		}
+
+		public String getTopic() {
+			return this.topic;
+		}
+		
+		public String toString() {
+			return String.format("Message [type=%s, user=%s, topic=%s]", this.getType(), this.getUser(), this.topic);
+		}
+		
+		//====================================================================
 		
 }

6c294cae53f4ccbc7c447089a66d3bea01fad551
Sondre <songro93@gmail.com>
2021-03-06 23:37:25 +0100

    PublishMessage Complete

--- src/no/hvl/dat110/messages/PublishMsg.java
+++ src/no/hvl/dat110/messages/PublishMsg.java
@@ -1,19 +1,37 @@
 package no.hvl.dat110.messages;
 
-import no.hvl.dat110.common.TODO;
-
 public class PublishMsg extends Message {
 	
 	// message sent from client to create publish a message on a topic 
 
-	// TODO:
+	// COMPLETED:
 	// Implement object variables - a topic and a message is required
 
 	// Constructor, get/set-methods, and toString method
 	// as described in the project text
 	
-	public String getMessage() {
-		
-		throw new UnsupportedOperationException(TODO.method());
+	//====================================================================
+	
+	private String topic;
+	private String message;
+	
+	public PublishMsg(String user, String topic, String message) {
+		super(MessageType.PUBLISH, user);
+		this.topic = topic;
+		this.message = message;
 	}
+	
+	public String getTopic() {
+		return this.topic;
+	}
+
+	public String getMessage() {
+		return this.message;
+	}
+
+	public String toString() {
+		return String.format("Message [type=%s, user=%s, topic=%s, message=%s]", this.getType(), this.getUser(), this.topic, this.message);
+	}
+	
+	//====================================================================
 }
